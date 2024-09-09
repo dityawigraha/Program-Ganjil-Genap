56c2ab9850eadf578dce9c4ec7ea70bf892ab346
Juana <inijuana@gmail.com>
2024-09-09 20:00:18 +0800

    initial commit

---
+++ Build-It.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int x;
+    scanf("%d",&x);
+    
+    if(x % 2)
+        printf("Ganjil");
+    else
+        printf("Genap");
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
