d95812dfba023f47f1390457c1ee10f802c01624
Juana <inijuana@gmail.com>
2024-09-09 19:14:23 +0800

    initial commit build it

---
+++ 1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    
+    printf("halo");
+
+    // printf("masukkan angka : ");  
+    // scanf("%i",&angka);
+    // printf("angka adalah %i", angka);
+    return 0;
+};
